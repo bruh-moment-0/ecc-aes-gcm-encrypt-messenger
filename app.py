4cbce2078af028dbe0e09ab9bdcfd48b79a64b69
bruhmmnt <162716920+bruh-moment-0@users.noreply.github.com>
2025-05-16 12:36:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -146,5 +146,7 @@ def user_remove():
 def index():
     return send_from_directory("static", "index.html")
 
-if __name__=="__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == "__main__":
+    import os
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
